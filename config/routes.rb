400b03fbc40719c862ed69fbf5d8e5d15ff1dff3
priyanka19697 <priyanka19697@gmail.com>
2019-10-23 16:36:25 +0530

    spelling mistake

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_page/home'
-  get 'static_page/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
