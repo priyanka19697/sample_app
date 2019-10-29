bc8973406da5535ed333e1019001c61d9c1f2a5b
priyanka19697 <priyanka19697@gmail.com>
2019-10-29 19:05:42 +0530

    implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if(user && user.authenticate(params[:session][:password]))
+      #login and redirect to user's show page
+      log_in(user)
+      redirect_to user
+    else
+      #create an error message - contents of flash.now disappear as soon as there is an additional request
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.find(params[:id])  
   end
+
+  def index 
+    @users = User.all
+  end
+
 
   def new
     @user = User.new
@@ -10,18 +15,22 @@ class UsersController < ApplicationController
   def create
     # @user = User.new(params[:user])
     @user = User.new(user_params)
+
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       #handle a successful save
       redirect_to user_url @user
     else 
       render 'new'
     end
+
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
 end
   
